648c78ac8442335ecba2f1ef1a3710942f195252
Nilesh Wankhede <wankhedenilesh2025@gmail.com>
2026-02-21 10:22:35 +0530

    Add morgan for logging and update package dependencies

--- src/app.js
+++ src/app.js
@@ -1,23 +1,27 @@
-const express = require("express");
-const cookieParser = require("cookie-parser");
-const cors = require("cors")
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const morgan = require('morgan');
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({
-    credentials : true, // used to allow set cookies on client-side [ credentials & origin is must ]
-    origin : "http://localhost:5173"
-}))
+app.use(morgan('dev')); // *
+app.use(
+  cors({
+    credentials: true, // used to allow set cookies on client-side [ credentials & origin is must ]
+    origin: 'http://localhost:5173',
+  }),
+);
 
 /* require routes */
-const authRouter = require("./routes/auth.routes")
-const postRouter = require("./routes/post.routes")
-const userRouter = require("./routes/user.routes")
+const authRouter = require('./routes/auth.routes');
+const postRouter = require('./routes/post.routes');
+const userRouter = require('./routes/user.routes');
 
 /* use routes */
-app.use("/api/auth",authRouter);
-app.use("/api/posts",postRouter);
-app.use("/api/users",userRouter);
+app.use('/api/auth', authRouter);
+app.use('/api/posts', postRouter);
+app.use('/api/users', userRouter);
 
 module.exports = app;

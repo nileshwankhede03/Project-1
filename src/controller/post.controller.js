f0c34c4c8d3e770f4bb0d525015623f313941612
Nilesh Wankhede <wankhedenilesh2025@gmail.com>
2026-02-28 21:52:48 +0530

    Add like status to posts in getFeed response

--- src/controller/post.controller.js
+++ src/controller/post.controller.js
@@ -116,7 +116,23 @@ async function likePostController(req, res) {
 }
 
 async function getFeedController(req, res) {
-  const posts = await postModel.find().populate('user');
+
+  const user = req.user;
+
+  const posts = await Promise.all((await postModel.find().populate('user').lean())
+                .map(async (post)=>{
+
+                  const isLiked = await likeModel.findOne({
+                    user : user.username,
+                    post : post._id
+                  })
+
+                  !!isLiked
+
+                  post.isLiked = !!isLiked
+
+                  return post
+                }))
 
   res.status(200).json({
     message: 'posts fetched successfully',
